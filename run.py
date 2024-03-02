4b00a26ef7a74d5a29f8e057603f8cf3a6ec2995
Scott Boulter <scottb@nofussit.co.uk>
2024-03-02 16:03:10 +0000

    Render testing

--- run.py
+++ run.py
@@ -1,10 +1,5 @@
 from app import create_app
 import os
-from dotenv import load_dotenv
-
-dotenv_path = '/app/.env'
-
-load_dotenv(dotenv_path)
 
 host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
 port = int(os.getenv('FLASK_RUN_PORT', 80))
