517324f495167353bf542027f0e094a8b4332fd4
Scott Boulter <scottb@nofussit.co.uk>
2024-03-02 14:09:23 +0000

    Dev build

--- run.py
+++ run.py
@@ -6,12 +6,12 @@ dotenv_path = '/app/.env'
 
 load_dotenv(dotenv_path)
 
+host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
+port = int(os.getenv('FLASK_RUN_PORT', 80))
+
+app = create_app()
+
+app.secret_key = os.getenv('SECRET_KEY')
+
 if __name__ == '__main__':
-    host = os.getenv('FLASK_RUN_HOST', '0.0.0.0')
-    port = int(os.getenv('FLASK_RUN_PORT', 80))
-
-    app = create_app()
-
-    app.secret_key = os.getenv('SECRET_KEY')
-
     app.run(host=host, port=port)
